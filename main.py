914e158ff6c85e8b66185b3ab1755f1a0f8e4872
Technisha <proxneon960@gmail.com>
2021-05-08 21:32:38 +0100

    Got the image to actually get drawn on the screen

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ from sys import argv
 
 from raylib.static import *
 
-from utils.helper import Screen, Fps
+from utils.helper import Screen, Fps, Vector2
+from utils.entities import Entity
 
 for arg in argv:
     argv[argv.index(arg)] = arg.lower()
@@ -19,11 +20,18 @@ except IndexError:
 SetTargetFPS(Fps.cap)
 InitWindow(Screen.width, Screen.height, b"Test Platformer")
 
+player = Entity('assets/player/walk.png', 256, 234, 4, 1, Vector2(424, 230))
+
 while not WindowShouldClose():
     BeginDrawing()
-    ClearBackground(RAYWHITE)
-    if fps:
+    if Fps.enabled:
         DrawFPS(Screen.width - 75, 0)
+    if IsMouseButtonPressed(MOUSE_LEFT_BUTTON):
+        print(f"X: {GetMouseX()}\nY: {GetMouseY()}")
+        player.pos.x = GetMouseX()
+        player.pos.y = GetMouseY()
+    player.draw()
+    ClearBackground(RAYWHITE)
     EndDrawing()
 
 CloseWindow()
--- utils/entities.py
+++ utils/entities.py
@@ -1,5 +1,7 @@
-from raylib.colors import RAYWHITE
-from raylib.static import LoadTexture, DrawTextureRec
+from copy import copy
+
+from raylib.colors import WHITE
+from raylib.static import LoadTexture, DrawTextureTiled
 
 from .helper import Vector2, Rectangle
 
@@ -7,23 +9,24 @@ ENTITIES = []
 
 
 class Entity:
-    def __init__(self, spritesheet, w_h, count, pos=Vector2(), hitbox=Rectangle()):
-        self.spritesheet = LoadTexture(spritesheet)
-        self.width = w_h[0]
-        self.length = w_h[1]
-        self.frames = count
-        self.current_frame = 1
-        self.pos = pos
+    def __init__(self, spritesheet, width, height, x_frames, y_frames, pos=Vector2(), hitbox=Rectangle()):
+        self.spritesheet = LoadTexture(spritesheet.encode())
+        self.width = width
+        self.height = height
+        self.single_frame = Rectangle(x=self.width / x_frames, y=self.height / y_frames, width=self.width / x_frames,
+                                      height=self.height / y_frames)
+        self.frames_num = x_frames * y_frames
+        self.current_frame = copy(self.single_frame)
+        self.current_frame_num = 1
+        self.pos = Rectangle(width=pos.x, height=pos.y)
         self.hitbox = hitbox
-        ENTITIES.append(self)
 
-    def __del__(self):
-        ENTITIES.remove(self)
-        super().__del__()
+    def drawIdle(self):
+        DrawTextureTiled(self.spritesheet, self.single_frame(), self.hitbox(), self.pos(), 0, 1, WHITE)
 
     def draw(self):
-        DrawTextureRec(self.spritesheet, self.hitbox, self.pos(), RAYWHITE)
-        if self.current_frame >= self.frames:
-            self.current_frame = 1
+        DrawTextureTiled(self.spritesheet, self.current_frame(), self.pos(), Vector2(self.pos.x, self.pos.y)(), 0, 1, WHITE)
+        if self.current_frame_num >= self.frames_num:
+            self.current_frame_num = 1
         else:
-            self.current_frame += 1
+            self.current_frame_num += 1
